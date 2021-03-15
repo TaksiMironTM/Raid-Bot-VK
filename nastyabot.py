a17b937247ebf3dbd88ca71594b4934d3b1954c9
TaksiMironTM <74793911+TaksiMironTM@users.noreply.github.com>
2021-03-15 22:34:18 +0500

    Add files via upload

---
+++ nastyabot.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import vk_api
+import vk
+import random
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import preview
+
+NASTYA = ["photo-140929_13730091", "photo559433773_457247408", "photo22505982_457239732"] #Можете добавлять свои фото - ["photo123_123", "photo123_123", "photo123_123", "photo123_123", "photo123_123"] - вот так
+
+
+vk_session = vk_api.VkApi(token='ТОКЕН') #Замените ТОКЕН на свой token от группы 
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session,123) #Замените 123 на свой ID группы 
+def main():
+
+	keyboard = VkKeyboard(one_time=False) 
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_line()	
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_line()
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.PRIMARY)
+	keyboard.add_line()
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.POSITIVE)
+	keyboard.add_line()
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.NEGATIVE)
+	keyboard.add_line()	
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.POSITIVE)
+	keyboard.add_line()
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.PRIMARY)
+	keyboard.add_line()
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_line()
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	
+	while True: 
+		try: 
+			for event in longpoll.listen():
+
+				if event.type == VkBotEventType.MESSAGE_NEW:
+					print("Настюху добавили в чат!!!")
+					while True:
+						vk.messages.send(peer_id=event.object.peer_id, attachment=random.choice(NASTYA), keyboard=keyboard.get_keyboard(), random_id=0)	
+					
+			
+		except Exception as e:
+			print('Настюха отработала!!!') 
+
+if __name__ == '__main__':
+	main()
