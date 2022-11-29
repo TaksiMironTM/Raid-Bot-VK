4e1ed4880da8290141edd110e85dcd0d7e25e24d
TaksiMironTM <74793911+TaksiMironTM@users.noreply.github.com>
2022-11-29 14:56:52 +0500

    Update nastyabot.py

--- nastyabot.py
+++ nastyabot.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import vk_api
-import vk
 import random
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
@@ -11,15 +9,15 @@ import preview
 NASTYA = ["photo-140929_13730091", "photo559433773_457247408", "photo22505982_457239732"] #Можете добавлять свои фото - ["photo123_123", "photo123_123", "photo123_123", "photo123_123", "photo123_123"] - вот так
 
 
-vk_session = vk_api.VkApi(token='ТОКЕН') #Замените ТОКЕН на свой token от группы 
+vk_session = vk_api.VkApi(token='ТОКЕН') #Замените ТОКЕН на token от группы 
 vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session,123) #Замените 123 на свой ID группы 
+longpoll = VkBotLongPoll(vk_session,123) #Замените 123 на ID от группы 
 def main():
 
 	keyboard = VkKeyboard(one_time=False) 
-	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.PRIMARY)
 	keyboard.add_line()	
-	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.NEGATIVE)
 	keyboard.add_line()
 	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.PRIMARY)
 	keyboard.add_line()
@@ -31,9 +29,9 @@ def main():
 	keyboard.add_line()
 	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.PRIMARY)
 	keyboard.add_line()
-	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.POSITIVE)
 	keyboard.add_line()
-	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.DEFAULT)
+	keyboard.add_button("NASTYA Shturm TaksiMironTM vto.pe", color=VkKeyboardColor.NEGATIVE)
 	
 	while True: 
 		try: 
